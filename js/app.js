4e908f41f911c4d06457550350aecdd490c42ce2
Jorge <jorrillop@hotmail.com>
2019-09-11 16:54:16 +0200

    primer commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const carrito = document.querySelector('#carrito')
+
+const listaCursos = document.querySelector('#lista-cursos')
+
+
+function addEventListeners(){
+
+document.querySelector()
+
+}
+
+addEventListeners();
